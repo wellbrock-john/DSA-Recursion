c0d171d24d9105492d3197eec74a3e899bfb789d
John Wellbrock <wellbrock23john@gmail.com>
2020-11-30 09:26:48 -0700

    problems 2 and 3 completed

--- DSA-Recursive.js
+++ DSA-Recursive.js
@@ -18,3 +18,33 @@ const countSheep = function (number) {
 
 let num = 3;
 console.log(countSheep(num));
+
+//2. POWER CALCULATOR
+const powerCalculator = function (baseInt, exponent) {
+	//Base case
+	if (exponent < 0) {
+		return "exponent should be >=0";
+	} else if (exponent === 0) {
+		return 1;
+	} else if (exponent === 1) {
+		return baseInt;
+	} else {
+		return baseInt * powerCalculator(baseInt, exponent - 1);
+	}
+};
+
+let base = 10;
+let exp = 4;
+console.log(powerCalculator(base, exp));
+
+//3. REVERSE STRING
+const reverseString = function (string) {
+	//Base case
+	if (string === "") {
+		return "";
+	} else {
+		return reverseString(string.substr(1)) + string.charAt(0);
+	}
+};
+
+console.log(reverseString("hello"));
